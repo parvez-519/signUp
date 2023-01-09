e4542404ffb0ab82470c75fa0d13134c37bedde2
Parvez Pathan <parvez.pathan.hyd@xelpmoc.in>
2023-01-09 22:37:10 +0530

    added common message in constants.js

--- api/constants.js
+++ api/constants.js
@@ -4,7 +4,13 @@ const appConst = {
     status:{
         success: 'SUCCESS',
         fail: 'FAIL',
-        in_progress:'IN PROGRESS'
+        in_progress:'IN PROGRESS',
+    },
+    message:{
+        pwd_pattern:'PASSWORD REQUIREMENT NOT MATCHED',
+        signup_success: 'SignUp Successfully',
+        user_not_found:"User not found",
+        pwd_successfully_changed:"Password successfully changed"
     },
     ROLE_ADMIN:'admin'
 }
