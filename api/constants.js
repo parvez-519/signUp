cb29e24344bcfab5fc29761599289352b06fe3bd
Parvez Pathan <parvez.pathan.hyd@xelpmoc.in>
2023-01-10 12:26:45 +0530

    minor changes

--- api/constants.js
+++ api/constants.js
@@ -7,7 +7,7 @@ const appConst = {
         in_progress:'IN PROGRESS',
     },
     message:{
-        pwd_pattern:'PASSWORD REQUIREMENT NOT MATCHED',
+        pwd_pattern:'The password does not meet the password policy requirements',
         signup_success: 'SignUp Successfully',
         user_not_found:"User not found",
         pwd_successfully_changed:"Password successfully changed"
